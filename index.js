8c8472105215ffeab7e58cfe23181f4da5f2e659
claurojass23 <128934493+claurojass23@users.noreply.github.com>
2023-10-27 16:51:47 -0500

    navegadorInterno

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById('descargarBtn').addEventListener('click', function() {
   });
   
   function descargarPDF() {
-    // Puedes usar una URL válida de un archivo PDF en lugar de 'url_del_pdf.pdf'
+    
     let pdfURL = '';
     let a = document.createElement('a');
     a.href = pdfURL;
